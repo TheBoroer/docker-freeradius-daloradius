19ebdea04cdb4135bd75b06624c33081592119a2
TheBoroer <me@bo.ro>
2020-06-08 22:06:06 -0400

    wip

--- run.sh
+++ run.sh
@@ -6,18 +6,20 @@ echo "start init"
 sed -i "s/^\/init.sh//" /run.sh
 
 # Update DB Values for FreeRadius
-sed -e 's/server = "localhost"/server = "'$MYSQL_HOST'"/g' /etc/freeradius/3.0/mods-available/sql.default > /etc/freeradius/3.0/mods-available/sql
-sed -e 's/#port = 3306/port = '$MYSQL_PORT'/g' /etc/freeradius/3.0/mods-available/sql.default > /etc/freeradius/3.0/mods-available/sql
-sed -e 's/login = "radius"/login = "'$MYSQL_USER'"/g' /etc/freeradius/3.0/mods-available/sql.default > /etc/freeradius/3.0/mods-available/sql
-sed -e 's/password = "radpass"/password = "'$MYSQL_PASS'"/g' /etc/freeradius/3.0/mods-available/sql.default > /etc/freeradius/3.0/mods-available/sql
-sed -e 's/radius_db = "radius"/radius_db = "'$MYSQL_DATABASE'"/g' /etc/freeradius/3.0/mods-available/sql.default > /etc/freeradius/3.0/mods-available/sql
+cp /etc/freeradius/3.0/mods-available/sql.default /etc/freeradius/3.0/mods-available/sql
+sed -e 's/server = "localhost"/server = "'$MYSQL_HOST'"/g' /etc/freeradius/3.0/mods-available/sql
+sed -e 's/#port = 3306/port = '$MYSQL_PORT'/g' /etc/freeradius/3.0/mods-available/sql
+sed -e 's/login = "radius"/login = "'$MYSQL_USER'"/g' /etc/freeradius/3.0/mods-available/sql
+sed -e 's/password = "radpass"/password = "'$MYSQL_PASS'"/g' /etc/freeradius/3.0/mods-available/sql
+sed -e 's/radius_db = "radius"/radius_db = "'$MYSQL_DATABASE'"/g' /etc/freeradius/3.0/mods-available/sql
 
 # Update DB Values or daloRADIUS
-sed -e "s/$configValues\['CONFIG_DB_HOST'\] = 'localhost';/$configValues\['CONFIG_DB_HOST'\] = '"$MYSQL_HOST"';/" /var/www/daloradius/library/daloradius.conf.php.default > /var/www/daloradius/library/daloradius.conf.php
-sed -e "s/$configValues\['CONFIG_DB_PORT'\] = '3306';/$configValues\['CONFIG_DB_PORT'\] = '"$MYSQL_PORT"';/" /var/www/daloradius/library/daloradius.conf.php.default > /var/www/daloradius/library/daloradius.conf.php
-sed -e "s/$configValues\['CONFIG_DB_USER'\] = 'root';/$configValues\['CONFIG_DB_USER'\] = '"$MYSQL_USER"';/" /var/www/daloradius/library/daloradius.conf.php.default > /var/www/daloradius/library/daloradius.conf.php
-sed -e "s/$configValues\['CONFIG_DB_PASS'\] = '';/$configValues\['CONFIG_DB_PASS'\] = '"$MYSQL_PASS"';/" /var/www/daloradius/library/daloradius.conf.php.default > /var/www/daloradius/library/daloradius.conf.php
-sed -e "s/$configValues\['CONFIG_DB_NAME'\] = 'radius';/$configValues\['CONFIG_DB_NAME'\] = '"$MYSQL_DATABASE"';/" /var/www/daloradius/library/daloradius.conf.php.default > /var/www/daloradius/library/daloradius.conf.php
+cp /var/www/daloradius/library/daloradius.conf.php.default /var/www/daloradius/library/daloradius.conf.php
+sed -i -e "s/\$configValues\['CONFIG_DB_HOST'\] = 'localhost';/\$configValues\['CONFIG_DB_HOST'\] = '"$MYSQL_HOST"';/" /var/www/daloradius/library/daloradius.conf.php
+sed -i -e "s/\$configValues\['CONFIG_DB_PORT'\] = '3306';/\$configValues\['CONFIG_DB_PORT'\] = '"$MYSQL_PORT"';/" /var/www/daloradius/library/daloradius.conf.php
+sed -i -e "s/\$configValues\['CONFIG_DB_USER'\] = 'root';/\$configValues\['CONFIG_DB_USER'\] = '"$MYSQL_USER"';/" /var/www/daloradius/library/daloradius.conf.php
+sed -i -e "s/\$configValues\['CONFIG_DB_PASS'\] = '';/\$configValues\['CONFIG_DB_PASS'\] = '"$MYSQL_PASS"';/" /var/www/daloradius/library/daloradius.conf.php
+sed -i -e "s/\$configValues\['CONFIG_DB_NAME'\] = 'radius';/\$configValues\['CONFIG_DB_NAME'\] = '"$MYSQL_DATABASE"';/" /var/www/daloradius/library/daloradius.conf.php
 
 mkdir /run/php & 
 php-fpm7.2 & 
